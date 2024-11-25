1e7a9c73dac74916fec05390330f98f18dde9281
SurajKushwaha <surajkushwaha21@gmail.com>
2024-11-25 20:27:49 +0530

    Update Lab001_Python_HelloWorld.py

---
+++ PythonCodes/ex_09112024_Literals_Variables/Lab024_Strings.py
@@ -0,0 +1,12 @@
+age = "90" # its a string
+age1 = 90 # its a Int type
+print(type(age))  # it should print the datatype as string
+print(type(age1)) # it should print data type as Int
+
+# str(), int(), float(), bool()
+
+
+# str age -> int
+print ("changing the datatype from string to int")
+age = int(age)
+print(type(age))
