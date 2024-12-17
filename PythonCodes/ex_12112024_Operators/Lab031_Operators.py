d317c82ba8a0125e0f475dc9a5dc5e92f81c44b8
SurajKushwaha <surajkushwaha21@gmail.com>
2024-12-17 05:47:17 +0530

    Update Lab001_Python_HelloWorld.py

--- PythonCodes/ex_12112024_Operators/Lab031_Operators.py
+++ PythonCodes/ex_12112024_Operators/Lab031_Operators.py
@@ -1,13 +1,21 @@
-age = 65
-print("it should be", age)
+# ✅ Operators
 
+# age = 65
+# age -> ref variable name | identifier
+# = -> operator
+# 65 -> literal | variable value
 
+# Unary - operator - which is added to 1 operand
 
+age = +65
+age2 = -65
 
+print(age)
+print(age2)
 
-
-
-print(13//2)
-print(13/2)
+# - - unary operator -
+how_many_lambo_pramod = -1
+how_many_lambo_pramod = how_many_lambo_pramod + 1
+print(how_many_lambo_pramod)
 
 
